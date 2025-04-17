5022ebe77edfac42f9409dfa33ec6bc8d5323737
sebastian-reyes <sebastianreyes2105@gmail.com>
2025-04-17 09:26:42 -0500

    chore: initial commit

---
+++ src/main/java/com/utp/reponse/BusinessParkingResponseApplication.java
@@ -0,0 +1,13 @@
+package com.utp.reponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessParkingResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessParkingResponseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utp/reponse/BusinessParkingResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.reponse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessParkingResponseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
